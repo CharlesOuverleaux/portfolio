63071beab230011ca9022e3233325b6f8dbf1537
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-02-28 23:28:36 +0100

    Switched views

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,27 +5,27 @@ export default function Header() {
   return (
     <>
       <div className="px-4 pt-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="flex flex-row sm:flex-col sm:w-[75px]">
+        <div className="flex flex-col w-[75px] sm:flex-row sm:w-full">
           <Image
             src="/C.svg"
             alt="C"
             width={75}
             height={75}
-            className="animate-introY1 sm:animate-introX1"
+            className="animate-introX1 sm:animate-introY1"
           />
           <Image
             src="/A.svg"
             alt="A"
             width={75}
             height={75}
-            className="animate-introY2 sm:animate-introX2"
+            className="animate-introX2 sm:animate-introY2"
           />
           <Image
             src="/O.svg"
             alt="O"
             width={75}
             height={75}
-            className="animate-introY3 sm:animate-introX3"
+            className="animate-introX3 sm:animate-introY3"
           />
         </div>
       </div>
