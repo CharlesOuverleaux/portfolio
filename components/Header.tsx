143ae226b9b1d681948c508d716559c0927fc218
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-03-10 23:13:30 +0100

    Added Tailwind nav and cross button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,35 +1,216 @@
 /* This example requires Tailwind CSS v2.0+ */
 import Image from "next/image";
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import React, { FC } from "react";
+
+const navigation = [
+  { name: "Work", href: "#", current: true },
+  { name: "About", href: "#", current: false },
+  { name: "Contact", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const NameInitials: FC = () => {
+  return (
+    <div className="px-4 pt-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div className="flex flex-row w-full">
+        <Image
+          src="/C.svg"
+          alt="C"
+          width={45}
+          height={75}
+          className="animate-introY1"
+        />
+        <Image
+          src="/A.svg"
+          alt="A"
+          width={45}
+          height={75}
+          className="animate-introY2"
+        />
+        <Image
+          src="/O.svg"
+          alt="O"
+          width={45}
+          height={75}
+          className="animate-introY3"
+        />
+      </div>
+      <div>Menu</div>
+    </div>
+  );
+};
 
 export default function Header() {
   return (
     <>
-      <div className="px-4 pt-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="flex flex-row w-full">
-          <Image
-            src="/C.svg"
-            alt="C"
-            width={45}
-            height={75}
-            className="animate-introY1"
-          />
-          <Image
-            src="/A.svg"
-            alt="A"
-            width={45}
-            height={75}
-            className="animate-introY2"
-          />
-          <Image
-            src="/O.svg"
-            alt="O"
-            width={45}
-            height={75}
-            className="animate-introY3"
-          />
-        </div>
-        <div>Menu</div>
-      </div>
+      <Disclosure as="nav" className="bg-gray-800">
+        {({ open }) => (
+          <>
+            <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center text-gray-400 rounded-full hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <Image
+                        src="/CrossButton.svg"
+                        alt="CrossButton"
+                        width={24}
+                        height={24}
+                        className="duration-500 ease-in-out rotate-45"
+                      />
+                    ) : (
+                      <Image
+                        src="/CrossButton.svg"
+                        alt="CrossButton"
+                        width={24}
+                        height={24}
+                        className="duration-500 ease-in-out"
+                      />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                  <div className="flex items-center flex-shrink-0">
+                    <img
+                      className="block w-auto h-8 lg:hidden"
+                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                      alt="Workflow"
+                    />
+                    <img
+                      className="hidden w-auto h-8 lg:block"
+                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                      alt="Workflow"
+                    />
+                  </div>
+                  <div className="hidden sm:block sm:ml-6">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}
+                          aria-current={item.current ? "page" : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <button
+                    type="button"
+                    className="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  >
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="w-6 h-6" aria-hidden="true" />
+                  </button>
+
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="w-8 h-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Your Profile
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Settings
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Sign out
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </>
   );
 }
