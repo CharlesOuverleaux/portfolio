b8d840f66ba42af54603eb9c6bbfb270b9a0f00e
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-03-13 16:30:20 +0100

    Added back of card animation

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,19 +3,45 @@ import { CardProps } from "../lib/types";
 import Image from "next/image";
 import Hover from "react-3d-hover";
 
+const CardBack: FC = () => {
+  return (
+    <div className="card-back">
+      <div className="relative group">
+        <div className="grid w-full overflow-hidden bg-[#183DF2] rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none">
+          <div className="relative w-5/6 font-bold text-white uppercase border-8 border-white place-self-center h-5/6">
+            <div className="absolute top-0 left-0 m-4">
+              <p>Frontend engineer</p>
+              <p>OneFootball</p>
+            </div>
+            <p className="absolute bottom-0 right-0 m-4 animate-introX1">
+              Read more
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const Card: FC<CardProps> = ({ product }) => {
   return (
     <div className="container" key={product.id}>
-      <Hover scale={1.05} perspective={900} speed={1000}>
-        <div className="relative group">
-          <div className="flex w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none justify-center self-center">
-            <Image
-              src={"/OneFootballLogo.svg"}
-              alt={"OneFootballLogo"}
-              width={250}
-              height={250}
-              className="hover:z-50"
-            />
+      <Hover>
+        <div className="group card-front">
+          <div className="relative group">
+            <div className="group-hover:hidden grid w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none justify-center self-center">
+              <div className="place-self-center">
+                <Image
+                  src={"/OneFootballLogo.svg"}
+                  alt={"OneFootballLogo"}
+                  width={250}
+                  height={250}
+                />
+              </div>
+            </div>
+            <div className="hidden group-hover:block">
+              <CardBack />
+            </div>
           </div>
         </div>
       </Hover>
