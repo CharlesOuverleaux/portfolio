c7b6ba9f065cfa7e762cc609a9e33cd8996ad2e4
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-03-13 15:15:10 +0100

    Added card 3d effect

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,18 +1,24 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import { CardProps } from "../lib/types";
 import Image from "next/image";
+import Hover from "react-3d-hover";
 
 export const Card: FC<CardProps> = ({ product }) => {
   return (
-    <div key={product.id} className="relative group">
-      <div className="flex w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none justify-center self-center">
-        <Image
-          src={"/OneFootballLogo.svg"}
-          alt={"OneFootballLogo"}
-          width={250}
-          height={250}
-        />
-      </div>
+    <div className="container" key={product.id}>
+      <Hover scale={1.05} perspective={900} speed={1000}>
+        <div className="relative group">
+          <div className="flex w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none justify-center self-center">
+            <Image
+              src={"/OneFootballLogo.svg"}
+              alt={"OneFootballLogo"}
+              width={250}
+              height={250}
+              className="hover:z-50"
+            />
+          </div>
+        </div>
+      </Hover>
     </div>
   );
 };
