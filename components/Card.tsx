c832a14a0e7cdcf05849d621e395a97fa5249bfd
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-03-13 17:19:19 +0100

    Optimized cards for viewports

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ import Hover from "react-3d-hover";
 
 const CardFront: FC = () => {
   return (
-    <div className="grid w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none justify-center self-center">
+    <div className="grid w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-square lg:h-80 lg:aspect-none justify-center self-center">
       <div className="place-self-center">
         <Image
           src={"/OneFootballLogo.svg"}
@@ -20,19 +20,15 @@ const CardFront: FC = () => {
 
 const CardBack: FC = () => {
   return (
-    <div className="card-back">
-      <div className="relative group">
-        <div className="grid w-full overflow-hidden bg-[#183DF2] rounded-md min-h-80 aspect-w-1 aspect-h-1 lg:h-80 lg:aspect-none">
-          <div className="relative w-5/6 font-bold text-white uppercase border-8 border-white place-self-center h-5/6">
-            <div className="absolute top-0 left-0 m-4 animate-introX2">
-              <p>Frontend engineer</p>
-              <p>OneFootball</p>
-            </div>
-            <p className="absolute bottom-0 right-0 m-4 animate-introX1">
-              Read more
-            </p>
-          </div>
+    <div className="grid w-full overflow-hidden bg-[#183DF2] rounded-md min-h-80 aspect-square lg:h-80 lg:aspect-none">
+      <div className="relative w-5/6 font-bold text-white uppercase border-8 border-white place-self-center h-5/6">
+        <div className="absolute top-0 left-0 m-4 animate-introX2">
+          <p>Frontend engineer</p>
+          <p>OneFootball</p>
         </div>
+        <p className="absolute bottom-0 right-0 m-4 animate-introX1">
+          Read more
+        </p>
       </div>
     </div>
   );
@@ -42,14 +38,12 @@ export const Card: FC<CardProps> = ({ product }) => {
   return (
     <div className="container" key={product.id}>
       <Hover>
-        <div className="group card-front">
-          <div className="relative group">
-            <div className="group-hover:hidden">
-              <CardFront />
-            </div>
-            <div className="hidden group-hover:block">
-              <CardBack />
-            </div>
+        <div className="relative shadow-2xl group card">
+          <div className="group-hover:hidden">
+            <CardFront />
+          </div>
+          <div className="hidden group-hover:block">
+            <CardBack />
           </div>
         </div>
       </Hover>
