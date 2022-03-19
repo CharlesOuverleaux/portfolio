96b420d4d6bc4b44636d1545a1294e9cdf6888fd
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-03-19 16:59:33 +0100

    Fix lg card size

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ import Hover from "react-3d-hover";
 
 const CardFront: FC<CardProps> = ({ job }) => {
   return (
-    <div className="grid w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md min-h-80 aspect-square lg:h-80 lg:aspect-none justify-center self-center">
+    <div className="grid w-full overflow-hidden bg-[url('/BlueTexture.svg')] bg-repeat rounded-md h-80 aspect-square justify-center self-center">
       <div className="place-self-center">
         <Image src={job.imageSrc} alt={job.imageAlt} width={250} height={250} />
       </div>
@@ -15,15 +15,15 @@ const CardFront: FC<CardProps> = ({ job }) => {
 
 const CardBack: FC<CardProps> = ({ job }) => {
   return (
-    <div className="grid w-full overflow-hidden bg-[#183DF2] rounded-md min-h-80 aspect-square lg:h-80 lg:aspect-none">
+    <div className="grid w-full overflow-hidden bg-[#183DF2] rounded-md h-80 aspect-square">
       <div className="relative w-5/6 font-bold text-white uppercase border-8 border-white place-self-center h-5/6">
         <div className="absolute top-0 left-0 m-4 animate-introX2">
           <p>{job.title}</p>
           <p>{job.company}</p>
         </div>
-        <p className="absolute bottom-0 right-0 m-4 animate-introX1">
-          Read more
-        </p>
+        <div className="absolute bottom-0 right-0 m-4 animate-introX1">
+          <p>Read more</p>
+        </div>
       </div>
     </div>
   );
@@ -32,16 +32,14 @@ const CardBack: FC<CardProps> = ({ job }) => {
 export const Card: FC<CardProps> = ({ job }) => {
   return (
     <div className="container" key={job.jobId}>
-      <Hover>
-        <div className="relative shadow-2xl group card">
-          <div className="group-hover:hidden">
-            <CardFront job={job} />
-          </div>
-          <div className="hidden group-hover:block">
-            <CardBack job={job} />
-          </div>
+      <div className="shadow-2xl group card">
+        <div className="block group-hover:hidden">
+          <CardFront job={job} />
         </div>
-      </Hover>
+        <div className="hidden group-hover:block">
+          <CardBack job={job} />
+        </div>
+      </div>
     </div>
   );
 };
