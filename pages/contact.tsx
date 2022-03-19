b6f4307b17ec471d74faaaca18ae5563eea2413b
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-03-19 20:40:20 +0100

    Added ref

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,5 @@
+// Ref: https://tailwindui.com/components/marketing/page-examples/contact-pages#component-b2b99a39b231970b58c51fb20e23953d
+
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Page } from "../components/Page";
