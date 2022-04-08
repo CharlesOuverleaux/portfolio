17101c095bc95e0585339fb526e40f9e95499d81
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-04-08 19:13:41 +0200

    Add sticky and text color effect

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,7 +11,14 @@ const About: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page>
-        <h1>About me</h1>
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          {/* Ref: https://daily-dev-tips.com/posts/css-text-color-difference-based-on-background/ */}
+          <div className="fixed w-screen h-screen text-white mix-blend-difference">
+            <h1 className="text-5xl uppercase">Charles Ouverleaux</h1>
+          </div>
+          <section className="w-screen h-screen bg-black"></section>
+          <section className="w-screen h-screen bg-white"></section>
+        </div>
       </Page>
     </div>
   );
