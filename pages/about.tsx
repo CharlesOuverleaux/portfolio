7055e67eacbbd86f1481e4c29da23e517d24fbbc
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-04-18 17:20:04 +0200

    Styled about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { Page } from "../components/Page";
-import Image from "next/image";
 
 const About: NextPage = () => {
   return (
@@ -14,19 +13,28 @@ const About: NextPage = () => {
       <Page>
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           {/* Ref: https://daily-dev-tips.com/posts/css-text-color-difference-based-on-background/ */}
-          <div className="fixed h-screen text-black max-w-7xl">
-            <p className="text-5xl uppercase bg-[url('/surf2.svg')] bg-no-repeat bg-cover bg-clip-text text-transparent">
+          <div className="fixed h-screen text-white max-w-7x mix-blend-difference">
+            <h1 className="py-12 tracking-widest uppercase text-3xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bebas bg-[url('/surf2.svg')] bg-bottom bg-cover bg-clip-text text-transparent">
+              Charles Ouverleaux <br />
               Building is more <br />
               than a passion
-            </p>
-            <div className="grid w-full grid-cols-3 pt-12 italic uppercase">
-              <p>Beautiful Frontend Applications</p>
-              <p>Innovative Tech Products</p>
-              <p>Sustainable Wooden Surfboard</p>
-            </div>
-            <h1 className="py-12 uppercase text-7xl font-bebas">
-              Charles Ouverleaux
             </h1>
+            <div className="w-full pt-12 sm:grid sm:grid-cols-12">
+              <p className="col-span-3 py-2 font-bold tracking-wide">
+                I am a Frontend engineer with a strong business background
+                (Amazon, Berlin start-ups, ICHEC Business School).
+              </p>
+              <p className="col-span-6 py-2">
+                Common Senses Festival, is a first-of-its-kind, festival that
+                explores the human senses through interactive installations,
+                live performances, and more. This month-long festival, is
+                presented by local non-profit Autism Action Partnership.
+              </p>
+              <p className="col-span-2 py-2">
+                LG, Hobbies (Sustainable Wooden Surfboard), Location, contact me
+                button
+              </p>
+            </div>
           </div>
           <div className="grid grid-cols-3">
             <div className="w-full h-48 col-span-1 bg-white"></div>
