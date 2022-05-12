5590e4b69632db4bd125cab6fd655f8d7a874e91
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-05-12 22:01:21 +0200

    Add profile grid

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,41 +12,35 @@ const About: NextPage = () => {
       </Head>
       <Page>
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          {/* Ref: https://daily-dev-tips.com/posts/css-text-color-difference-based-on-background/ */}
-          <div className="fixed h-screen text-white max-w-7x mix-blend-difference">
-            <h1 className="py-12 tracking-widest uppercase text-3xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-bebas bg-[url('/surf2.svg')] bg-bottom bg-cover bg-clip-text text-transparent">
-              Charles Ouverleaux <br />
-              Building is more <br />
-              than a passion
-            </h1>
-            <div className="w-full pt-12 sm:grid sm:grid-cols-12">
-              <p className="col-span-3 py-2 font-bold tracking-wide">
-                I am a Frontend engineer with a strong business background
-                (Amazon, Berlin start-ups, ICHEC Business School).
-              </p>
-              <p className="col-span-6 py-2">
-                Common Senses Festival, is a first-of-its-kind, festival that
-                explores the human senses through interactive installations,
-                live performances, and more. This month-long festival, is
-                presented by local non-profit Autism Action Partnership.
-              </p>
-              <p className="col-span-2 py-2">
-                LG, Hobbies (Sustainable Wooden Surfboard), Location, contact me
-                button
-              </p>
+          <div className="grid-cols-4 grid-rows-5 sm:grid">
+            <div className="bg-[#4895ef] ">Color 1</div>
+            <div className="bg-[#3a0ca3]">Color 2</div>
+            <div className="bg-[#7209b7]">Color 3</div>
+            <div className="bg-[#f72585]">Color 4</div>
+            <div className="col-start-1 col-end-3 row-start-2 row-end-4">
+              Programming
             </div>
-          </div>
-          <div className="grid grid-cols-3">
-            <div className="w-full h-48 col-span-1 bg-white"></div>
-            <div className="w-full h-48 col-span-1 bg-white"></div>
-            <div className="w-full h-48 col-span-1 bg-black"></div>
-            <div className="w-full col-span-1 bg-white h-96"></div>
-            <div className="w-full col-span-1 bg-black h-96"></div>
-            <div className="w-full col-span-1 bg-white h-96"></div>
-            <div className="w-full h-48 col-span-1 bg-black"></div>
-            <div className="w-full h-48 col-span-1 bg-white"></div>
-            <div className="w-full h-48 col-span-1 bg-white"></div>
-            <div className="w-full h-screen col-span-3 bg-white"></div>
+            <div className="col-start-3 col-end-4 row-start-2 row-end-3">
+              Github
+            </div>
+            <div className="col-start-3 col-end-4 row-start-3 row-end-4">
+              Twitter
+            </div>
+            <div className="col-start-4 col-end-5 row-start-2 row-end-4">
+              Dream big
+            </div>
+            <div className="col-start-1 col-end-2 row-start-4 row-end-6">
+              Surfing
+            </div>
+            <div className="col-start-2 col-end-3 row-start-4 row-end-5">
+              Find your passion
+            </div>
+            <div className="col-start-2 col-end-3 row-start-5 row-end-6">
+              Music
+            </div>
+            <div className="col-start-3 col-end-5 row-start-4 row-end-6">
+              Woodworking
+            </div>
           </div>
         </div>
       </Page>
