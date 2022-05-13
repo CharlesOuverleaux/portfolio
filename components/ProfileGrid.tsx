14a63249ce9ed30f6b862c4987e356e8a1f09abc
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-05-13 16:49:54 +0200

    Fill grid with content

--- components/ProfileGrid.tsx
+++ components/ProfileGrid.tsx
@@ -2,16 +2,22 @@ import React, { FC } from "react";
 import Image from "next/image";
 import programmingPic from "../public/profile/Programming.png";
 import fuelPic from "../public/profile/Fuel.png";
+import dreamBigPic from "../public/profile/DreamBig.png";
+import woodWorkingPic from "../public/profile/WoodWorking.png";
+import githubPic from "../public/profile/Github.png";
+import twitterPic from "../public/profile/Twitter.png";
+import surfingPic from "../public/profile/Surfing.png";
+import musicPic from "../public/profile/Music.png";
 
 export const ProfileGrid: FC = () => {
   return (
     <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div className="grid-cols-4 grid-rows-5 sm:grid">
-        <div className="bg-[#4895ef] ">Color 1</div>
-        <div className="bg-[#3a0ca3]">Color 2</div>
-        <div className="bg-[#7209b7]">Color 3</div>
-        <div className="bg-[#f72585]">Color 4</div>
-        <div className="relative h-[267px] col-start-1 col-end-3 row-start-2 row-end-4">
+        <div className="bg-[#4895ef] h-12">Color 1</div>
+        <div className="bg-[#3a0ca3] h-12">Color 2</div>
+        <div className="bg-[#7209b7] h-12">Color 3</div>
+        <div className="bg-[#f72585] h-12">Color 4</div>
+        <div className="relative h-full col-start-1 col-end-3 row-start-2 row-end-4">
           <div className="absolute z-10 flex items-center justify-center w-full h-full">
             <h2 className="text-2xl font-bold text-white uppercase">
               Programming
@@ -29,21 +35,62 @@ export const ProfileGrid: FC = () => {
           />
         </div>
         <div className="col-start-3 col-end-4 row-start-2 row-end-3">
-          Github
+          <Image
+            src={githubPic}
+            alt="GitHub"
+            layout="responsive"
+            width={512}
+            height={524}
+            objectFit="cover"
+            objectPosition="center"
+            placeholder="blur"
+          />
         </div>
         <div className="col-start-3 col-end-4 row-start-3 row-end-4">
-          Twitter
+          <Image
+            src={twitterPic}
+            alt="Twitter"
+            layout="responsive"
+            width={512}
+            height={512}
+            objectFit="cover"
+            objectPosition="center"
+            placeholder="blur"
+          />
         </div>
-        <div className="col-start-4 col-end-5 row-start-2 row-end-4">
-          Dream big
+        <div className="relative col-start-4 col-end-5 row-start-2 row-end-4">
+          <div className="absolute z-10 flex items-center justify-center w-full h-full">
+            <h2 className="text-2xl font-bold text-white uppercase">
+              NFT - WEB3
+            </h2>
+          </div>
+          <Image
+            src={dreamBigPic}
+            alt="Dream Big"
+            layout="responsive"
+            width={578}
+            height={626}
+            objectFit="cover"
+            objectPosition="center"
+            placeholder="blur"
+          />
         </div>
         <div className="col-start-1 col-end-2 row-start-4 row-end-6">
-          Surfing
+          <Image
+            src={surfingPic}
+            alt="surfing"
+            layout="responsive"
+            width={674}
+            height={522}
+            objectFit="cover"
+            objectPosition="center"
+            placeholder="blur"
+          />
         </div>
         <div className="col-start-2 col-end-3 row-start-4 row-end-5">
           <Image
             src={fuelPic}
-            alt="Programming"
+            alt="Fuel your passion"
             layout="responsive"
             width={592}
             height={268}
@@ -52,9 +99,29 @@ export const ProfileGrid: FC = () => {
             placeholder="blur"
           />
         </div>
-        <div className="col-start-2 col-end-3 row-start-5 row-end-6">Music</div>
+        <div className="col-start-2 col-end-3 row-start-5 row-end-6">
+          <Image
+            src={musicPic}
+            alt="Music"
+            layout="responsive"
+            width={590}
+            height={536}
+            objectFit="cover"
+            objectPosition="center"
+            placeholder="blur"
+          />
+        </div>
         <div className="col-start-3 col-end-5 row-start-4 row-end-6">
-          Woodworking
+          <Image
+            src={woodWorkingPic}
+            alt="Woodworking"
+            layout="responsive"
+            width={592}
+            height={268}
+            objectFit="cover"
+            objectPosition="center"
+            placeholder="blur"
+          />
         </div>
       </div>
     </div>
