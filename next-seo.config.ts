20b50914f7990e27becf78b5d8654b0aff532e53
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-05-14 18:23:53 +0200

    Add next seo

---
+++ next-seo.config.ts
@@ -0,0 +1,46 @@
+// Ref: https://nextjs.org/learn/seo/rendering-and-ranking/metadata
+// Ref: https://www.npmjs.com/package/next-seo#user-content-default-seo-configuration
+// SEO default config using next-seo, can be overriden on a page by page basis.
+// TODO: Add final image, twitter account
+import { DefaultSeoProps } from "next-seo/lib/types";
+
+const siteName = "charlesouverleaux.co";
+const title = "Charles Ouverleaux | Frontend Engineer";
+const description =
+  "From business to tech. Frontend engineer with business and marketing background";
+const keywords = [
+  "Frontend engineer",
+  "Web developement",
+  "Freelance",
+  "NFT",
+  "Web3",
+];
+
+const seoConfig: DefaultSeoProps = {
+  title: title,
+  titleTemplate: `${siteName} | %s`,
+  description: description,
+  openGraph: {
+    title: `${siteName} | ${title}`,
+    description: description,
+    type: "website",
+    locale: "en_IE",
+    images: [
+      {
+        url: "/ProfilePicture_Charles.jpg",
+        width: 600,
+        height: 600,
+        alt: "Charles Ouverleaux Frontend Engineer",
+        type: "image/jpg",
+      },
+    ],
+    site_name: siteName,
+  },
+  twitter: {
+    handle: "@CO_IN_TECH",
+    site: "@charlesouverleaux.co",
+    cardType: "summary_large_image",
+  },
+  additionalMetaTags: [{ name: "keywords", content: keywords.join(",") }],
+};
+export default seoConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next/seo";
+import seoConfig from "../next-seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return;
+  <DefaultSeo {...seoConfig}>
+    <Component {...pageProps} />
+  </DefaultSeo>;
 }
 
-export default MyApp
+export default MyApp;
