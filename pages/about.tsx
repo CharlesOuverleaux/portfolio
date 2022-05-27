3c79ad6065b9c1520d42fa9d9c40cef7ce910c8d
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2022-05-27 17:17:36 +0200

    Remove Breadcrums JsonLd

--- pages/about.tsx
+++ pages/about.tsx
@@ -31,26 +31,6 @@ const About: NextPage<PageProps> = ({ canonicalUrl }) => {
           },
         }}
       />
-      <ProfilePageJsonLd
-        lastReviewed="2022-05-24T19:30"
-        breadcrumb={[
-          {
-            position: 1,
-            name: "charlesouverleaux.co",
-            item: "https://www.charlesouverleaux.co",
-          },
-          {
-            position: 2,
-            name: "about",
-            item: "https://www.charlesouverleaux.co/about",
-          },
-          {
-            position: 3,
-            name: "contact",
-            item: "https://www.charlesouverleaux.co/contact",
-          },
-        ]}
-      />
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
